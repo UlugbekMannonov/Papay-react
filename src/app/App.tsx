91f7fc33904aa879c171628c347a0b1492f72894
UlugbekMannonov <ulugbek.sbk.2021@gmail.com>
2023-06-27 17:48:24 +0800

    feat: customized integration of MUI

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,27 @@
+import { Button, Container, Typography } from '@mui/material';
+import { Box, Stack } from '@mui/system';
 import React from 'react';
 import '../css/App.css';
+import { RippleBadge } from './MaterialTheme/styled';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Container maxWidth="sm">
+      <Stack flexDirection={"column"}>
+        <Box sx={{my: 4}}>
+          <Typography variant='h4' component={'h1'} gutterBottom>
+            Create React App on TypeScript with REDUX
+          </Typography>
+        </Box>
+        <Box>
+          <RippleBadge badgeContent={4}>
+            <Button color='secondary' variant="contained">Contained</Button>
+
+          </RippleBadge>
+        </Box>
+      </Stack>
+    </Container>
+    
   );
 }
 
