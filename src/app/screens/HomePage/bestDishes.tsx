38c462ab507d967514292dea377117db85dab193
UlugbekMannonov <ulugbek.sbk.2021@gmail.com>
2023-07-03 23:05:44 +0800

    feat: homepage ads section publishing

--- src/app/screens/HomePage/bestDishes.tsx
+++ src/app/screens/HomePage/bestDishes.tsx
@@ -1,10 +1,124 @@
-import { Container } from "@mui/material";
+import { MonetizationOn } from "@mui/icons-material";
+import { Box, Container, Stack } from "@mui/material";
 import React from "react";
 
 export function BestDishes() {
   return (
-    <div className="top_restaurant_frame">
-      <Container>BestDishes</Container>
+    <div className="best_dishes_frame">
+      <Container>
+        <Stack flexDirection={"column"} alignItems={"center"}>
+          <Box className="category_title">Trenddagi Ovqatlar</Box>
+          <Stack sx={{ mt: "43px" }} flexDirection={"row"}>
+            <Box className="dish_box">
+              <Stack
+                className="dish_img"
+                sx={{
+                  backgroundImage: `url(
+                    "https://www.lacademie.com/wp-content/uploads/2022/03/uzbek-foods.jpg"
+                  )`,
+                }}
+              >
+                <div className={"dish_sale"}>normal size</div>
+                <div className={"view_btn"}>
+                  Batfsil Ko'rish{" "}
+                  <img
+                    src={"/icons/arrow_right.svg"}
+                    style={{ marginLeft: "9px" }}
+                  />
+                </div>
+              </Stack>
+              <Stack className={"dish_desc"}>
+                <span className={"dish_title_text"}>Chicken Mayo</span>
+                <span className={"dish_desc_text"}>
+                  <MonetizationOn />
+                  11
+                </span>
+              </Stack>
+            </Box>
+
+            <Box className="dish_box">
+              <Stack
+                className="dish_img"
+                sx={{
+                  backgroundImage: `url(
+                    "https://www.lacademie.com/wp-content/uploads/2022/03/uzbek-foods.jpg"
+                  )`,
+                }}
+              >
+                <div className={"dish_sale"}>normal size</div>
+                <div className={"view_btn"}>
+                  Batfsil Ko'rish{" "}
+                  <img
+                    src={"/icons/arrow_right.svg"}
+                    style={{ marginLeft: "9px" }}
+                  />
+                </div>
+              </Stack>
+              <Stack className={"dish_desc"}>
+                <span className={"dish_title_text"}>Chicken Mayo</span>
+                <span className={"dish_desc_text"}>
+                  <MonetizationOn />
+                  11
+                </span>
+              </Stack>
+            </Box>
+
+            <Box className="dish_box">
+              <Stack
+                className="dish_img"
+                sx={{
+                  backgroundImage: `url(
+                    "https://www.lacademie.com/wp-content/uploads/2022/03/uzbek-foods.jpg"
+                  )`,
+                }}
+              >
+                <div className={"dish_sale"}>normal size</div>
+                <div className={"view_btn"}>
+                  Batfsil Ko'rish{" "}
+                  <img
+                    src={"/icons/arrow_right.svg"}
+                    style={{ marginLeft: "9px" }}
+                  />
+                </div>
+              </Stack>
+              <Stack className={"dish_desc"}>
+                <span className={"dish_title_text"}>Chicken Mayo</span>
+                <span className={"dish_desc_text"}>
+                  <MonetizationOn />
+                  11
+                </span>
+              </Stack>
+            </Box>
+
+            <Box className="dish_box">
+              <Stack
+                className="dish_img"
+                sx={{
+                  backgroundImage: `url(
+                    "https://www.lacademie.com/wp-content/uploads/2022/03/uzbek-foods.jpg"
+                  )`,
+                }}
+              >
+                <div className={"dish_sale"}>normal size</div>
+                <div className={"view_btn"}>
+                  Batfsil Ko'rish{" "}
+                  <img
+                    src={"/icons/arrow_right.svg"}
+                    style={{ marginLeft: "9px" }}
+                  />
+                </div>
+              </Stack>
+              <Stack className={"dish_desc"}>
+                <span className={"dish_title_text"}>Chicken Mayo</span>
+                <span className={"dish_desc_text"}>
+                  <MonetizationOn />
+                  11
+                </span>
+              </Stack>
+            </Box>
+          </Stack>
+        </Stack>
+      </Container>
     </div>
   );
 }
