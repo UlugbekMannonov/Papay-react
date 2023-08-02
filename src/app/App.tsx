db8892e7bcae8cf7fee546916358571a71326b87
UlugbekMannonov <ulugbek.sbk.2021@gmail.com>
2023-08-02 20:35:17 +0800

    feat: create react class component example

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -20,6 +20,7 @@ import { NavbarHome } from "./components/header";
 import { NavbarRestaurant } from "./components/header/restaurant";
 import { NavbarOthers } from "./components/header/others";
 import { Footer } from "./components/footer";
+import Car from './screens/testCar';
 
 
 function App() {
@@ -56,6 +57,7 @@ function App() {
               <LoginPage />
             </Route>
             <Route path="/">
+              {/* <Car /> */}
               <HomePage />
             </Route>
         </Switch>
