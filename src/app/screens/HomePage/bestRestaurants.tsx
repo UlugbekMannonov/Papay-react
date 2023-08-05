e2906a6e0afa7c82b8894c1fdcac7994390e332b
UlugbekMannonov <ulugbek.sbk.2021@gmail.com>
2023-08-05 18:31:29 +0800

    feat: retrieve best restaurants data

--- src/app/screens/HomePage/bestRestaurants.tsx
+++ src/app/screens/HomePage/bestRestaurants.tsx
@@ -1,393 +1,159 @@
-import React from "react";
+import React from 'react';
 import {
-  AspectRatio,
-  Card,
-  CardOverflow,
-  CssVarsProvider,
-  IconButton,
-  Link,
-  Typography,
-} from "@mui/joy";
-import { Box, Button, Container, Stack } from "@mui/material";
-import Favorite from "@mui/icons-material/Favorite";
-import LocationOnRoundedIcon from "@mui/icons-material/LocationOnRounded";
-import CallIcon from "@mui/icons-material/Call";
-import VisibilityIcon from "@mui/icons-material/Visibility";
-import { SentimentNeutral } from "@mui/icons-material";
+	AspectRatio,
+	Card,
+	CardOverflow,
+	CssVarsProvider,
+	IconButton,
+	Link,
+	Typography,
+} from '@mui/joy';
+import { Box, Button, Container, Stack } from '@mui/material';
+import Favorite from '@mui/icons-material/Favorite';
+import LocationOnRoundedIcon from '@mui/icons-material/LocationOnRounded';
+import CallIcon from '@mui/icons-material/Call';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import { SentimentNeutral } from '@mui/icons-material';
+
+//REDUX
+import { useSelector } from 'react-redux';
+import { createSelector } from 'reselect';
+import { retrieveBestRestaurants } from '../../screens/HomePage/selector';
+import { Restaurant } from '../../../types/user';
+import { serviceApi } from '../../../lib/config';
+
+//** REDUX SELECTOR */
+const bestRestaurantsRetriever = createSelector(
+	retrieveBestRestaurants,
+	(bestRestaurants) => ({
+		bestRestaurants,
+	})
+);
 
 export function BestRestaurants() {
-  return (
-    <div className="best_restaurant_frame">
-      <img
-        src={"icons/line_group.svg"}
-        style={{
-          position: "absolute",
-          left: "6%",
-          transform: "rotate(90degree",
-        }}
-      />
-      <Container sx={{ paddingTop: "153px" }}>
-        <Stack flexDirection={"column"} alignItems={"center"}>
-          <Box className="category_title">Zo'r Restaurantlar</Box>
-          <Stack sx={{ mt: "43px" }} flexDirection={"row"}>
-            <CssVarsProvider>
-              <Card
-                variant="outlined"
-                sx={{ minHeight: 483, minWidth: 320, mr: "35px" }}
-              >
-                <CardOverflow>
-                  <AspectRatio ratio="1">
-                    <img src={"restaurant/burak.jpeg"} />
-                  </AspectRatio>
-                  <IconButton
-                    aria-label="Like minimal photography"
-                    size="md"
-                    variant="solid"
-                    color="danger"
-                    sx={{
-                      position: "absolute",
-                      zIndex: 2,
-                      borderRadius: "50%",
-                      right: "1rem",
-                      bottom: 0,
-                      transform: "translateY(50%)",
-                      color: "rgba(0,0,0,.4)",
-                    }}
-                  >
-                    <Favorite /* @ts-ignore */ style={{ color: "white" }} />
-                  </IconButton>
-                </CardOverflow>
-                <Typography level="h2" sx={{ fontSize: "md", mt: 2 }}>
-                  Texas De Brazil
-                </Typography>
-                <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
-                  <Link
-                    href=""
-                    startDecorator={<LocationOnRoundedIcon />}
-                    textColor="neutral.700"
-                  >
-                    Tashkent Yunusabad
-                  </Link>
-                </Typography>
-                <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
-                  <Link
-                    href=""
-                    startDecorator={<CallIcon />}
-                    textColor="neutral.700"
-                  >
-                    998990144544
-                  </Link>
-                </Typography>
-                <CardOverflow
-                  sx={{
-                    display: "flex",
-                    gap: 1.5,
-                    py: 1.5,
-                    px: "var(--Card--padding",
-                    borderTop: "1px solid",
-                    borderColor: "neutral.outlineBorder",
-                    bgcolor: "background.level1",
-                  }}
-                >
-                  <Typography
-                    level="body3"
-                    sx={{
-                      fontweight: "md",
-                      color: "text.secondary",
-                      alignItems: "center",
-                      display: "flex",
-                    }}
-                  >
-                    100{""}
-                    <VisibilityIcon sx={{ fontSize: 20, marginLeft: "5px" }} />
-                  </Typography>
-                  <Box sx={{ width: 2, bgcolor: "divider" }} />
-                  <Typography
-                    level="body3"
-                    sx={{
-                      fontweight: "md",
-                      color: "text.secondary",
-                      alignItems: "center",
-                      display: "flex",
-                    }}
-                  >
-                    <div>50</div>
-                    <Favorite sx={{ fontSize: 20, marginLeft: "5px" }} />
-                  </Typography>
-                </CardOverflow>
-              </Card>
+	// INITIALIZATION
+	const { bestRestaurants } = useSelector(bestRestaurantsRetriever);
+	return (
+		<div className="best_restaurant_frame">
+			<img
+				src={'icons/line_group.svg'}
+				style={{
+					position: 'absolute',
+					left: '6%',
+					transform: 'rotate(90degree',
+				}}
+			/>
+			<Container sx={{ paddingTop: '153px' }}>
+				<Stack flexDirection={'column'} alignItems={'center'}>
+					<Box className="category_title">Zo'r Restaurantlar</Box>
+					<Stack sx={{ mt: '43px' }} flexDirection={'row'}>
+						{bestRestaurants.map((ele: Restaurant) => {
+							const image_path = `${serviceApi}/${ele.mb_image}`;
+							return (
+								<CssVarsProvider>
+									<Card
+										variant="outlined"
+										sx={{ minHeight: 483, minWidth: 320, mr: '35px' }}
+									>
+										<CardOverflow>
+											<AspectRatio ratio="1">
+												<img src={image_path} />
+											</AspectRatio>
+											<IconButton
+												aria-label="Like minimal photography"
+												size="md"
+												variant="solid"
+												color="danger"
+												sx={{
+													position: 'absolute',
+													zIndex: 2,
+													borderRadius: '50%',
+													right: '1rem',
+													bottom: 0,
+													transform: 'translateY(50%)',
+													color: 'rgba(0,0,0,.4)',
+												}}
+											>
+												<Favorite /* @ts-ignore */ style={{ color: 'white' }} />
+											</IconButton>
+										</CardOverflow>
+										<Typography level="h2" sx={{ fontSize: 'md', mt: 2 }}>
+											{ele.mb_nick} restaurant
+										</Typography>
+										<Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
+											<Link
+												href=""
+												startDecorator={<LocationOnRoundedIcon />}
+												textColor="neutral.700"
+											>
+												{ele.mb_address}
+											</Link>
+										</Typography>
+										<Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
+											<Link
+												href=""
+												startDecorator={<CallIcon />}
+												textColor="neutral.700"
+											>
+												{ele.mb_phone}
+											</Link>
+										</Typography>
+										<CardOverflow
+											sx={{
+												display: 'flex',
+												gap: 1.5,
+												py: 1.5,
+												px: 'var(--Card--padding',
+												borderTop: '1px solid',
+												borderColor: 'neutral.outlineBorder',
+												bgcolor: 'background.level1',
+											}}
+										>
+											<Typography
+												level="body3"
+												sx={{
+													fontweight: 'md',
+													color: 'text.secondary',
+													alignItems: 'center',
+													display: 'flex',
+												}}
+											>
+												{ele.mb_views}
+												<VisibilityIcon
+													sx={{ fontSize: 20, marginLeft: '5px' }}
+												/>
+											</Typography>
+											<Box sx={{ width: 2, bgcolor: 'divider' }} />
+											<Typography
+												level="body3"
+												sx={{
+													fontweight: 'md',
+													color: 'text.secondary',
+													alignItems: 'center',
+													display: 'flex',
+												}}
+											>
+												<div>{ele.mb_likes}</div>
+												<Favorite sx={{ fontSize: 20, marginLeft: '5px' }} />
+											</Typography>
+										</CardOverflow>
+									</Card>
+								</CssVarsProvider>
+							);
+						})}
+					</Stack>
 
-              <Card
-                variant="outlined"
-                sx={{ minHeight: 483, minWidth: 320, mr: "35px" }}
-              >
-                <CardOverflow>
-                  <AspectRatio ratio="1">
-                    <img src={"restaurant/burak.jpeg"} />
-                  </AspectRatio>
-                  <IconButton
-                    aria-label="Like minimal photography"
-                    size="md"
-                    variant="solid"
-                    color="danger"
-                    sx={{
-                      position: "absolute",
-                      zIndex: 2,
-                      borderRadius: "50%",
-                      right: "1rem",
-                      bottom: 0,
-                      transform: "translateY(50%)",
-                      color: "rgba(0,0,0,.4)",
-                    }}
-                  >
-                    <Favorite /* @ts-ignore */ style={{ color: "white" }} />
-                  </IconButton>
-                </CardOverflow>
-                <Typography level="h2" sx={{ fontSize: "md", mt: 2 }}>
-                  Texas De Brazil
-                </Typography>
-                <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
-                  <Link
-                    href=""
-                    startDecorator={<LocationOnRoundedIcon />}
-                    textColor="neutral.700"
-                  >
-                    Tashkent Yunusabad
-                  </Link>
-                </Typography>
-                <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
-                  <Link
-                    href=""
-                    startDecorator={<CallIcon />}
-                    textColor="neutral.700"
-                  >
-                    998990144544
-                  </Link>
-                </Typography>
-                <CardOverflow
-                  sx={{
-                    display: "flex",
-                    gap: 1.5,
-                    py: 1.5,
-                    px: "var(--Card--padding",
-                    borderTop: "1px solid",
-                    borderColor: "neutral.outlineBorder",
-                    bgcolor: "background.level1",
-                  }}
-                >
-                  <Typography
-                    level="body3"
-                    sx={{
-                      fontweight: "md",
-                      color: "text.secondary",
-                      alignItems: "center",
-                      display: "flex",
-                    }}
-                  >
-                    100{""}
-                    <VisibilityIcon sx={{ fontSize: 20, marginLeft: "5px" }} />
-                  </Typography>
-                  <Box sx={{ width: 2, bgcolor: "divider" }} />
-                  <Typography
-                    level="body3"
-                    sx={{
-                      fontweight: "md",
-                      color: "text.secondary",
-                      alignItems: "center",
-                      display: "flex",
-                    }}
-                  >
-                    <div>50</div>
-                    <Favorite sx={{ fontSize: 20, marginLeft: "5px" }} />
-                  </Typography>
-                </CardOverflow>
-              </Card>
-
-              <Card
-                variant="outlined"
-                sx={{ minHeight: 483, minWidth: 320, mr: "35px" }}
-              >
-                <CardOverflow>
-                  <AspectRatio ratio="1">
-                    <img src={"restaurant/burak.jpeg"} />
-                  </AspectRatio>
-                  <IconButton
-                    aria-label="Like minimal photography"
-                    size="md"
-                    variant="solid"
-                    color="danger"
-                    sx={{
-                      position: "absolute",
-                      zIndex: 2,
-                      borderRadius: "50%",
-                      right: "1rem",
-                      bottom: 0,
-                      transform: "translateY(50%)",
-                      color: "rgba(0,0,0,.4)",
-                    }}
-                  >
-                    <Favorite /* @ts-ignore */ style={{ color: "white" }} />
-                  </IconButton>
-                </CardOverflow>
-                <Typography level="h2" sx={{ fontSize: "md", mt: 2 }}>
-                  Texas De Brazil
-                </Typography>
-                <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
-                  <Link
-                    href=""
-                    startDecorator={<LocationOnRoundedIcon />}
-                    textColor="neutral.700"
-                  >
-                    Tashkent Yunusabad
-                  </Link>
-                </Typography>
-                <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
-                  <Link
-                    href=""
-                    startDecorator={<CallIcon />}
-                    textColor="neutral.700"
-                  >
-                    998990144544
-                  </Link>
-                </Typography>
-                <CardOverflow
-                  sx={{
-                    display: "flex",
-                    gap: 1.5,
-                    py: 1.5,
-                    px: "var(--Card--padding",
-                    borderTop: "1px solid",
-                    borderColor: "neutral.outlineBorder",
-                    bgcolor: "background.level1",
-                  }}
-                >
-                  <Typography
-                    level="body3"
-                    sx={{
-                      fontweight: "md",
-                      color: "text.secondary",
-                      alignItems: "center",
-                      display: "flex",
-                    }}
-                  >
-                    100{""}
-                    <VisibilityIcon sx={{ fontSize: 20, marginLeft: "5px" }} />
-                  </Typography>
-                  <Box sx={{ width: 2, bgcolor: "divider" }} />
-                  <Typography
-                    level="body3"
-                    sx={{
-                      fontweight: "md",
-                      color: "text.secondary",
-                      alignItems: "center",
-                      display: "flex",
-                    }}
-                  >
-                    <div>50</div>
-                    <Favorite sx={{ fontSize: 20, marginLeft: "5px" }} />
-                  </Typography>
-                </CardOverflow>
-              </Card>
-
-              <Card
-                variant="outlined"
-                sx={{ minHeight: 483, minWidth: 320, mr: "35px" }}
-              >
-                <CardOverflow>
-                  <AspectRatio ratio="1">
-                    <img src={"restaurant/burak.jpeg"} />
-                  </AspectRatio>
-                  <IconButton
-                    aria-label="Like minimal photography"
-                    size="md"
-                    variant="solid"
-                    color="danger"
-                    sx={{
-                      position: "absolute",
-                      zIndex: 2,
-                      borderRadius: "50%",
-                      right: "1rem",
-                      bottom: 0,
-                      transform: "translateY(50%)",
-                      color: "rgba(0,0,0,.4)",
-                    }}
-                  >
-                    <Favorite /* @ts-ignore */ style={{ color: "white" }} />
-                  </IconButton>
-                </CardOverflow>
-                <Typography level="h2" sx={{ fontSize: "md", mt: 2 }}>
-                  Texas De Brazil
-                </Typography>
-                <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
-                  <Link
-                    href=""
-                    startDecorator={<LocationOnRoundedIcon />}
-                    textColor="neutral.700"
-                  >
-                    Tashkent Yunusabad
-                  </Link>
-                </Typography>
-                <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
-                  <Link
-                    href=""
-                    startDecorator={<CallIcon />}
-                    textColor="neutral.700"
-                  >
-                    998990144544
-                  </Link>
-                </Typography>
-                <CardOverflow
-                  sx={{
-                    display: "flex",
-                    gap: 1.5,
-                    py: 1.5,
-                    px: "var(--Card--padding",
-                    borderTop: "1px solid",
-                    borderColor: "neutral.outlineBorder",
-                    bgcolor: "background.level1",
-                  }}
-                >
-                  <Typography
-                    level="body3"
-                    sx={{
-                      fontweight: "md",
-                      color: "text.secondary",
-                      alignItems: "center",
-                      display: "flex",
-                    }}
-                  >
-                    100{""}
-                    <VisibilityIcon sx={{ fontSize: 20, marginLeft: "5px" }} />
-                  </Typography>
-                  <Box sx={{ width: 2, bgcolor: "divider" }} />
-                  <Typography
-                    level="body3"
-                    sx={{
-                      fontweight: "md",
-                      color: "text.secondary",
-                      alignItems: "center",
-                      display: "flex",
-                    }}
-                  >
-                    <div>50</div>
-                    <Favorite sx={{ fontSize: 20, marginLeft: "5px" }} />
-                  </Typography>
-                </CardOverflow>
-              </Card>
-            </CssVarsProvider>
-          </Stack>
-
-          <Stack
-            flexDirection={"row"}
-            justifyContent={"flex-end"}
-            style={{ width: "100%", marginTop: "16px" }}
-          >
-            <Button style={{ background: "#1976D2", color: "#FFFFFF" }}>
-              Barchasini Ko'rish
-            </Button>
-          </Stack>
-        </Stack>
-      </Container>
-    </div>
-  );
+					<Stack
+						flexDirection={'row'}
+						justifyContent={'flex-end'}
+						style={{ width: '100%', marginTop: '16px' }}
+					>
+						<Button style={{ background: '#1976D2', color: '#FFFFFF' }}>
+							Barchasini Ko'rish
+						</Button>
+					</Stack>
+				</Stack>
+			</Container>
+		</div>
+	);
 }
--- src/app/screens/HomePage/index.tsx
+++ src/app/screens/HomePage/index.tsx
@@ -13,7 +13,10 @@ import '../../../css/home.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { Dispatch } from '@reduxjs/toolkit';
 import { createSelector } from 'reselect';
-import { setTopRestaurants } from '../../screens/HomePage/slice';
+import {
+	setBestRestaurants,
+	setTopRestaurants,
+} from '../../screens/HomePage/slice';
 import { retrieveTopRestaurants } from '../../screens/HomePage/selector';
 import { Restaurant } from '../../../types/user';
 import RestaurantApiService from '../../apiServices/restaurantApiService';
@@ -21,6 +24,8 @@ import RestaurantApiService from '../../apiServices/restaurantApiService';
 //** REDUX SLICE */
 const actionDispatch = (dispatch: Dispatch) => ({
 	setTopRestaurants: (data: Restaurant[]) => dispatch(setTopRestaurants(data)),
+	setBestRestaurants: (data: Restaurant[]) =>
+		dispatch(setBestRestaurants(data)),
 });
 
 //** REDUX SELECTOR */
@@ -35,7 +40,9 @@ const topRestaurantsRetriever = createSelector(
 export function HomePage() {
 	// selector: store => data
 	// INITIALIZATION
-	const { setTopRestaurants } = actionDispatch(useDispatch());
+	const { setTopRestaurants, setBestRestaurants} = actionDispatch(
+		useDispatch()
+	);
 
 	useEffect(() => {
 		// backend data request => data
@@ -46,6 +53,12 @@ export function HomePage() {
 				setTopRestaurants(data);
 			})
 			.catch((err) => console.log(err));
+			restaurantService
+				.getRestaurants({ page: 1, limit: 4, order: 'mb_point' })
+				.then((data) => {
+					setBestRestaurants(data);
+				})
+				.catch((err) => console.log(err));
 	}, []);
 
 	return (
