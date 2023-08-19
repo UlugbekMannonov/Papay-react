4db91bf095ef212c59cf8f7c284178116d3a4de7
UlugbekMannonov <ulugbek.sbk.2021@gmail.com>
2023-08-19 19:04:25 +0800

    feat: MemberPage settings logic development

--- src/app/screens/MemberPage/mySettings.tsx
+++ src/app/screens/MemberPage/mySettings.tsx
@@ -2,13 +2,86 @@ import React, { useEffect, useRef, useState } from "react";
 import { Box, Container, Stack } from "@mui/material";
 import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
 import Button from "@mui/material/Button";
+import { verifiedMemberData } from "../../apiServices/verify";
+import { MemberUpdateData } from "../../../types/user";
+import assert from "assert";
+import { Definer } from "../../../lib/Definer";
+import {
+  sweetErrorHandling,
+  sweetTopSmallSuccessAlert,
+} from "../../../lib/sweetAlert";
+import MemberAipService from "../../apiServices/memberApiService";
 
 export function MySettings(props: any) {
+  /** INITIALIZATIONS */
+  const [file, setFile] = useState(verifiedMemberData?.mb_image);
+
+  const [memberUpdate, setMemberUpdate] = useState<MemberUpdateData>({
+    mb_nick: "",
+    mb_phone: "",
+    mb_address: "",
+    mb_description: "",
+    mb_image: "",
+  });
+
+  /** HANDLERS */
+  const changeMemberNickHandler = (e: any) => {
+    memberUpdate.mb_nick = e.target.value;
+    setMemberUpdate({ ...memberUpdate });
+  };
+  const changeMemberPhoneHandler = (e: any) => {
+    memberUpdate.mb_phone = e.target.value;
+    setMemberUpdate({ ...memberUpdate });
+  };
+  const changeMemberAddressHandler = (e: any) => {
+    memberUpdate.mb_address = e.target.value;
+    setMemberUpdate({ ...memberUpdate });
+  };
+  const changeMemberDescriptionHandler = (e: any) => {
+    memberUpdate.mb_description = e.target.value;
+    setMemberUpdate({ ...memberUpdate });
+  };
+
+  const handleImagePreviewer = (e: any) => {
+    try {
+      const file = e.target.files[0];
+
+      const fileType = file["type"],
+        validTypes = ["image/jpg", "image/jpeg", "image/png"];
+      assert.ok(validTypes.includes(fileType) && file, Definer.input_err2);
+
+      memberUpdate.mb_image = file;
+      setMemberUpdate({ ...memberUpdate });
+      setFile(URL.createObjectURL(file));
+    } catch (err: any) {
+      console.log(`ERROR ::: handleImagePreviewer, ${err}`);
+      sweetErrorHandling(err).then();
+    }
+  };
+
+  const handleSubmitButton = async () => {
+    try {
+      const memberService = new MemberAipService();
+      const result = await memberService.updateMemberData(memberUpdate);
+
+      assert.ok(result, Definer.general_err1);
+      await sweetTopSmallSuccessAlert(
+        "Information modified successfully!",
+        700,
+        false
+      );
+      window.location.reload();
+    } catch (err) {
+      console.log(`ERROR ::: handleSubmitButton, ${err}`);
+      sweetErrorHandling(err).then();
+    }
+  };
+
   return (
     <Stack className={"my_settings_page"}>
       <Box className={"member_media_frame"}>
         <img
-          src={"/auth/default_user.svg"}
+          src={file}
           className={"mb_image"}
           style={{ borderRadius: "50%" }}
           width={"100px"}
@@ -18,7 +91,11 @@ export function MySettings(props: any) {
           <span>Rasm Yuklash</span>
           <p>JPG, JPEG, PNG rasmlarini yuklay olasiz!</p>
           <div className={"up_del_box"}>
-            <Button component="label" style={{ minWidth: "0" }}>
+            <Button
+              component="label"
+              style={{ minWidth: "0" }}
+              onChange={handleImagePreviewer}
+            >
               <CloudDownloadIcon />
               <input type="file" hidden />
             </Button>
@@ -31,8 +108,9 @@ export function MySettings(props: any) {
           <input
             className={"spec_input mb_nick"}
             type="text"
-            placeholder="Mannonov Ulugbek"
+            placeholder={verifiedMemberData?.mb_nick}
             name="mb_nick"
+            onChange={changeMemberNickHandler}
           />
         </div>
       </Box>
@@ -42,8 +120,9 @@ export function MySettings(props: any) {
           <input
             className={"spec_input mb_phone"}
             type="text"
-            placeholder={"99899 0144544"}
+            placeholder={verifiedMemberData?.mb_phone}
             name="mb_phone"
+            onChange={changeMemberPhoneHandler}
           />
         </div>
         <div className={"short_input"}>
@@ -51,8 +130,11 @@ export function MySettings(props: any) {
           <input
             className={"spec_input  mb_address"}
             type="text"
-            placeholder={"Tashkent, Yunus Abad 4-1"}
+            placeholder={
+              verifiedMemberData?.mb_address ?? "Manzil kiritilmagan"
+            }
             name="mb_address"
+            onChange={changeMemberAddressHandler}
           />
         </div>
       </Box>
@@ -61,13 +143,16 @@ export function MySettings(props: any) {
           <label className={"spec_label"}>Ma'lumot</label>
           <textarea
             className={"spec_textarea mb_description"}
-            placeholder={"Mavjud emas"}
+            placeholder={verifiedMemberData?.mb_description ?? "mavjud emas"}
             name="mb_description"
+            onChange={changeMemberDescriptionHandler}
           />
         </div>
       </Box>
       <Box display={"flex"} justifyContent={"flex-end"} sx={{ mt: "25px" }}>
-        <Button variant={"contained"}>Saqlash</Button>
+        <Button variant={"contained"} onClick={handleSubmitButton}>
+          Saqlash
+        </Button>
       </Box>
     </Stack>
   );
